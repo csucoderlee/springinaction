2b70be534098bdd88fe450e59cc6c662dc5b1597
lixiang <csucoderlee@gmail.com>
2017-01-19 17:01:33 +0800

    第四章开始

--- README.md
+++ README.md
@@ -7,6 +7,14 @@
 ### 本章主要讲述了bean的基本装配知识, 1.构造器注入 2.设值注入 3.工厂注入
 
 
-## com.springinaction.three
+## com.springinaction.three 最小化xml配置代码
+
+1.自动装配 2.自动检测
 
 ### 此package下的代码内容来源于 spring in action 的练习代码
+
+### 本章主要讲述了bean的自动装配、注解装配，着重在于注解装配
+
+注解装配：1.可选的装配 @Autowired(required=false) 2.限定歧义性依赖 @Qualified("guitar")
+
+## com.springaction.four 声明切面
---
+++ src/main/java/com/springinaction/four/Audience.java
@@ -0,0 +1,23 @@
+package com.springinaction.four;
+
+/**
+ * Created by lixiang on 17/1/19.
+ */
+public class Audience {
+
+    public void takeSeats(){
+        System.out.println("take seats");
+    }
+
+    public void turnOffCellPhones(){
+        System.out.println("turn off cell phones");
+    }
+
+    public void applaud(){
+        System.out.println("clap clap clap");
+    }
+
+    public void demandRefund(){
+        System.out.println("we want our money back");
+    }
+}
