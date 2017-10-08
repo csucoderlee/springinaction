20ce417ee7916050965dcc6e5a398e0a6870090f
lixiang <csucoderlee@gmail.com>
2017-10-08 11:48:41 +0800

    bean的作用域

--- src/main/java/com/springinaction/ch02/Ticket.java
+++ src/main/java/com/springinaction/ch02/Ticket.java
@@ -1,5 +1,8 @@
 package com.springinaction.ch02;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by lixiang on 2017 10 08 11:32.
  */
@@ -13,5 +16,20 @@ public class Ticket {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-beans.xml");
+        Ticket ticket = (Ticket) ctx.getBean("ticket");
+        System.out.println(ticket);
+
+        Ticket ticket1 = (Ticket) ctx.getBean("ticket");
+        System.out.println(ticket1);
+
+        Ticket tmp1 = (Ticket) ctx.getBean("ticket1");
+        System.out.println(tmp1);
+
+        Ticket tmp2 = (Ticket) ctx.getBean("ticket2");
+        System.out.println(tmp2);
     }
 }
