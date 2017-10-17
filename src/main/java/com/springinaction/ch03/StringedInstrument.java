1f97034550c721bd810f139da60cb2c1e52be2dc
lixiang <csucoderlee@gmail.com>
2017-10-17 22:54:12 +0800

    添加新注解

---
+++ src/main/java/com/springinaction/ch03/StringedInstrument.java
@@ -0,0 +1,17 @@
+package com.springinaction.ch03;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by lixiang on 2017 10 17 22:52.
+ */
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface StringedInstrument {
+}
